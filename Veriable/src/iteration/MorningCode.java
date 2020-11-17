89e54e5f88c2a3d07e705d9d8ed88a0ec5e70469
admin <admin@YD03-06>
2020-11-17 10:36:54 +0900

    스캐너

--- Veriable/src/iteration/MorningCode.java
+++ Veriable/src/iteration/MorningCode.java
@@ -3,14 +3,23 @@ package iteration;
 public class MorningCode {
 	public static void main(String[] args) {
 
-		int sum7 = 0;
+		int sum7 = 7;
 
 		for (int i = 1; i <= 50; i++) {
-			if (sum7 % 7 == 0) {
+			if(i%sum7 == 0) {
+				System.out.println(i + " 는 7의 배수입니다.");
 			}
-				sum7 = +i;
 		}
-				System.out.println(sum7);
+		
+		int sum8 = 8;
+		
+		for (int j =1; j<= 50; j++) {
+			if(j % sum8 == 0) {
+				System.out.println(j + " 는 8의 배수입니다.");
+			}
+		}
+		
+		
 	}
 
 }
