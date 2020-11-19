c009e1f929cdce46a2aeecb9a43f183c637a92ec
mangg <mangg@DESKTOP-JV15FUT>
2020-11-19 21:01:04 +0900

    첫번째

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,35 @@
+package helloWorld;
+
+public class HelloWorld { public static void main(String[] args) {
+			String name = "Hong Kil dong";
+			name = "홍길동";
+			System.out.println("My name is " + name);
+			int age = 20;
+			System.out.println("My age is " + age);
+			
+			int height = 170;
+			System.out.println("My height is " + height);
+			
+			System.out.println( name + " height is " + height );
+			
+			name = "김다현";
+			age = 23;
+			height = 161;
+			System.out.println(name + " " + "키는 " + height + " 입니다.");
+			
+			String name1 = "김몽";
+			int age1 = 1;
+			int height1 = 45;
+			
+			System.out.println(name1 + "의" + " 나이는 " + age1 + "이고" + " 키는 " + height1 + " 입니다." );
+			
+			System.out.println("둘의 나이의 합은 " + (age + age1) + " 입니다.");
+			
+			
+
+	
+
+}
+
+}
+
---
+++ helloWorld/src/helloWorld/Veriable.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class Veriable {
+	public static void main(String[] args) {
+		
+		int sum = 10 + 20;
+		System.out.println(10 + 20 + "sum" + sum + 10 + 20);
+
+
+	}
+
+}
